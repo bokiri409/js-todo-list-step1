48f603c5554571ecd29f827e36fb9641a4dae69d
bokiri <bokiri16@gmail.com>
2021-01-25 03:07:31 +0900

    [미션1] - 2. todolist 체크박스 클릭하여 complete상태로 변경

--- js/List.js
+++ js/List.js
@@ -2,6 +2,7 @@
 const taskInput = document.querySelector('.new-todo'); //input tag
 const todoList = document.querySelector('ul.todo-list');
 
+
 showTask(); 
 
 //if user entered
@@ -29,7 +30,8 @@ function get_list() { //input event
     }
 }
 
-function showTask() { //show new tasks
+//show new tasks
+function showTask() { 
     var getLocalStorage = localStorage.getItem("New Task"); //getting localstorage
     if (getLocalStorage == null) {
         listArr = []; //creating blank array
@@ -38,16 +40,40 @@ function showTask() { //show new tasks
     }
 
     var newliTag = '';
-    listArr.forEach(element => {
+    listArr.forEach((element, index) => {
         newliTag += `<li>
         <div class="view">
-          <input class="toggle" type="checkbox" />
+          <input class="toggle" type="checkbox" onclick="completeTask()"/>
           <label class="label"> ${ element } </label>
-          <button class="destroy"></button>
+          <button class="destroy" onclick="deleteTask()"></button>
         </div>
         <input class="edit" value="새로운 타이틀" />
       </li>`;
     });
     todoList.innerHTML = newliTag; //adding new li tag inside ul tag
 
+}
+
+var chk = false;
+//completed task function
+function completeTask(index) {
+    // 체크하면 completeArr 배열로 옮기게 하자
+    // var getLocalStorage = localStorage.getItem("New Task");
+    // completeArr = JSON.parse(getLocalStorage);
+    // completeArr.
+    
+    const checkbox = document.querySelectorAll('div.view .toggle'); // input checkbox
+    
+    for (var i = 0; i < checkbox.length; i++) {
+        if (checkbox[i].checked ) { //if task is done
+            checkbox[i].closest("li").classList.add("completed");
+        }
+        else {
+            checkbox[i].closest("li").classList.remove("completed");
+        }
+    }
+}
+
+function deleteTask() {
+    
 }
